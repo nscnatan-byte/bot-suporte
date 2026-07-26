38d070f9816d1569c00187267c1783d7151088cf
nscnatan-byte <nscnatan@gmail.com>
2026-07-26 20:04:15 -0300

    Update start.py

--- start.py
+++ start.py
@@ -6,16 +6,16 @@ def rodar_script(nome_arquivo):
 
 if __name__ == "__main__":
     print("🚀 Iniciando os dois bots juntos...")
-    
+
     # Inicia o bot de pagamento
     p1 = rodar_script("bot.py")
-    
+
     # Aguarda 2 segundos para organizar
     time.sleep(2)
-    
+
     # Inicia o bot de suporte
     p2 = rodar_script("suporte_bot.py")
-    
+
     # Mantém ambos rodando
     p1.wait()
     p2.wait()
